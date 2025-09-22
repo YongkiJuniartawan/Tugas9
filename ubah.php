2bf225f01adb2314bb60a82f4ab3dad332f968f5
Your Name <you@example.com>
2025-09-22 11:11:19 +0700

    komit final

--- ubah.php
+++ ubah.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$koneksi = mysqli_connect("localhost", "root", "", "db_xirpl1_34_1");
+$koneksi = mysqli_connect("localhost", "xirpl1-34", "3093736893", "db_xirpl1-34_1");
 $id = $_GET['id'];
 $data = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM Jual_ikan WHERE id_ikan='$id'"));
 
